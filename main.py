c3c7b6b9498b4d04c1ff5ed7d07ad2b6acb9d030
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2023-11-03 23:15:56 +0100

    spown new number + winning & game over condition

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ class GameBoard:
     def __init__(self, size):
         self.size = size
         self.grid = [[0] * size for _ in range(size)]
+        self.empty_cells = [1] * (size * size) # it is 1 becaus eif all are 0 it is game over
 
+#########################################################################################
+    
     def update(self):
         for el in self.grid:
             print(el)
 
-    def move_tile(self):
+#########################################################################################
+    
+    def read_user_input(self):
          while True:
             if keyboard.is_pressed('left'):
                 self.user_input = 'left'
@@ -27,25 +32,89 @@ class GameBoard:
                 self.user_input = 'up'
                 break
             time.sleep(0.1)
+    
+#########################################################################################
 
+    def spown_new(self):
+        # Define the possible outcomes and their corresponding weights
+        outcomes = [2, 4]
+        weights = [9, 1]  # 90% chance of 2, 10% chance of 4
+        # Use random.choices() to select a number based on the defined weights
+        number = random.choices(outcomes, weights)[0]
 
+        # track all empty cells (0) and save them in an array 
+        # the 'empty_cells' array will contain cordinates for row and column
+        self.empty_cells = [None] * (self.size * self.size)
+        index = 0  # Index to update the empty_cells list
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[row])):
+                if self.grid[row][column] ==  0:
+                    self.empty_cells[index] = [row, column]
+                    index += 1
+        # Filter out the None values in the empty_cells list
+        self.empty_cells = [cell for cell in self.empty_cells if cell is not None]
+        # chose a cordinate where we will put number
+        row, column = random.choice(self.empty_cells)
+        # delete that cordinate from self.empty_cells
+        self.empty_cells.remove([row, column])
+        #print(self.empty_cells)
+
+        # Place the new number in the selected position
+        self.grid[row][column] = number
+    
+#########################################################################################
+
+    def move_tile(self):
+        pass
+
+#########################################################################################
     def merge_tiles(self, tile1, tile2):
         # Merge two tiles with the same value
         pass
 
+#########################################################################################
+
     def is_game_over(self):
-        # Check if the game is over
-        pass
+        if any(self.empty_cells):
+            return False
+        print("GAME OVER")
+        return True
+
+#########################################################################################
 
     def is_game_won(self):
-        # Check if the player has won
-        pass
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[row])):
+                if self.grid[row][column] ==  2048:
+                    print("YOW WIN")
+                    return True
+        return False
+
+#########################################################################################
+
+
+
+
+
+# initialize array
+print("You can chose the size of your grid")
+print("How much do you want ot challeng your-self?")
+print("(Usually the grid is a 4x4)")
+size = int(input("So... what is the size of the grid:\t"))
+game_board = GameBoard(size)
 
 # Main game loop
-game_board = GameBoard(5)
 while not game_board.is_game_over() and not game_board.is_game_won():
     ### Handle user input and game logic
+    # print the grid
     game_board.update()
-    #while True:
-    game_board.move_tile()
+
+    # detect desire moovment
+    game_board.read_user_input()
     print(game_board.user_input)
+
+    # spown new number (2 or 4) in the grid
+    game_board.spown_new()
+
+    time.sleep(0.5)
+game_board.update()
