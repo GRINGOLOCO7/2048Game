8114c2c941fd757cf9f4c6ff6ce06bc3f770694b
Svrubio7 <70824463+Svrubio7@users.noreply.github.com>
2023-11-17 13:44:00 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,28 +1,31 @@
 class Node:
-    def __init__(self, value, score, next):  # Add score parameter
+    def __init__(self, value, next, score):  # self is the instance of this class
         self.value = value
-        self.score = score
         self.next = next
+        self.score = score
 
+ 
 class Stack:
     def __init__(self):
         self.top = None
+        self.score = 0
 
-    def push(self, value, score):  # Add score parameter
-        node = Node(value, score, self.top)
+    def push(self, value, score):
+        node = Node(value, self.top, score)
         self.top = node
+        self.top.score = score
+        self.score = score
 
     def pop(self):
         if not self.top:
-            return None, None  # Return a tuple of None values
-        value = self.top.value
-        score = self.top.score
+            return
+        value= self.top
         self.top = self.top.next
-        return value, score  # Return a tuple of value and score
+        
+        return value.value, value.score
 
-stack = Stack()
-stack.push(3)
-stack.push(4)
+
+
 #stack.pop() #--> 4
 #print(stack.pop()) #--> 3
 #stack.pop() #--> nothing
